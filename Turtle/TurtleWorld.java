a589b123da0de7d3a4e23bc831ba3b8256808836
jldeahr <jldeahr50@gmail.com>
2014-08-26 12:20:09 -0500

    Completed Chapter 1 Exercises
    Completed Chapter 1 Exercises, Testing the sync button.

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -3,11 +3,19 @@ import java.awt.Color;
 public class TurtleWorld
 {
     public static void main(String args[])
+            throws InterruptedException
     {
         World turtleWorld = new World();
-        Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        Turtle turtle = new Turtle(turtleWorld);        
+        turtle.setPenColor(Color.BLUE);
+        turtle.setPenWidth(20);
+        Thread.sleep(1000); // sleep 1000 milliseconds (1 second)
         turtle.penDown();
         turtle.forward(50);
+        turtle.penUp();
+       
+        turtle.penDown();
+        turtle.backward(50);
     }
 }
+
